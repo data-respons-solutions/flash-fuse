218077b6f0332a98bc55c4070e885e1b26a39928
Mikko Salomäki <ms@datarespons.se>
2022-10-06 09:46:57 +0200

    fuse read/write: use lseek for proper operation with nvram file

--- flash-fuse-common.cpp
+++ flash-fuse-common.cpp
@@ -7,47 +7,51 @@
 #include <cerrno>
 #include <cstdio>
 #include "flash-fuse-common.h"
+#include "log.h"
 
 static std::array<uint8_t, 4> read_fuse(const std::string& path, int offset)
 {
 	int fd = open(path.c_str(), O_RDONLY);
-	if (fd < 0) {
+	if (fd < 0)
 		throw std::runtime_error(std::string("Failed opening for reading: ") + path + ": " + std::to_string(errno));
+	if (lseek(fd, offset * 4, SEEK_SET) == (off_t) - 1) {
+		close(fd);
+		throw std::runtime_error(std::string("Failed seeking offset: ") + path + ": " + std::to_string(errno));
 	}
 	std::array<uint8_t, 4> buf {};
-	const ssize_t bytes = pread(fd, buf.data(), buf.size(), offset);
+	const ssize_t bytes = read(fd, buf.data(), buf.size());
 	const int read_errno = errno;
 	const int r = close(fd);
-	if (bytes < 0 || static_cast<std::size_t>(bytes) != buf.size()) {
+	if (bytes < 0 || static_cast<std::size_t>(bytes) != buf.size())
 		throw std::runtime_error(std::string("Failed reading: ") + path + ": " + std::to_string(read_errno));
-	}
-	if (r != 0) {
+	if (r != 0)
 		throw std::runtime_error(std::string("Failed closing: ") + path + ": " + std::to_string(errno));
-	}
 	return buf;
 }
 
 static void write_fuse(const std::string& path, int offset, const std::array<uint8_t, 4>& buf)
 {
 	int fd = open(path.c_str(), O_WRONLY);
-	if (fd < 0) {
+	if (fd < 0)
 		throw std::runtime_error(std::string("Failed opening for reading: ") + path + ": " + std::to_string(errno));
+	if (lseek(fd, offset * 4, SEEK_SET) == (off_t) - 1) {
+		close(fd);
+		throw std::runtime_error(std::string("Failed seeking offset: ") + path + ": " + std::to_string(errno));
 	}
-	const ssize_t bytes = pwrite(fd, buf.data(), buf.size(), offset);
+	const ssize_t bytes = write(fd, buf.data(), buf.size());
 	const int write_errno = errno;
 	const int r = close(fd);
-	if (bytes < 0 || static_cast<std::size_t>(bytes) != buf.size()) {
+	if (bytes < 0 || static_cast<std::size_t>(bytes) != buf.size())
 		throw std::runtime_error(std::string("Failed writing: ") + path + ": " + std::to_string(write_errno));
-	}
-	if (r != 0) {
+	if (r != 0)
 		throw std::runtime_error(std::string("Failed closing: ") + path + ": " + std::to_string(errno));
-	}
 }
 
 static uint32_t read_bank(const std::string& nvmem, int offset)
 {
 	const std::array<uint8_t, 4> buf = read_fuse(nvmem, offset);
 	const uint32_t val = buf.at(0) | buf.at(1) << 8 | buf.at(2) << 16 | buf.at(3) << 24;
+	pr_dbg("read: 0x%08x at offset 0x%x from %s\n", val, offset, nvmem.c_str());
 	return val;
 }
 FlagFuse::FlagFuse(std::string nvmem, int offset, uint32_t mask, std::map<std::string, uint32_t> flags)
